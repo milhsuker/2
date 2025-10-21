ab9cb79ef46a52fcc04a27129c3273e5aab8de97
milhsuker <milhsuker@gmail.com>
2025-10-21 10:17:29 +0300

    Fix: Change minifier from terser to esbuild for successful build

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       },
       build: {
         sourcemap: false,
-        minify: 'terser',
+        minify: 'esbuild',
         rollupOptions: {
           output: {
             manualChunks: undefined,
